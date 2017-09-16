e914e50956392221007753a8d441a222fa63ffda
gregpaton08 <gregpaton08@gmail.com>
2017-09-16 10:38:00 -0400

    Add test function for is door locked function

--- lock_motor.py
+++ lock_motor.py
@@ -1,8 +1,45 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
+import time
 
 MOTOR_LOCK_PIN = 4
-MOTOR_UNLOCK_PIN = 18 # still need to solder and hook this up
-MOTOR_SWITCH_PIN = 17 # Butterfly switch that signals when the lock is opened/closed
+MOTOR_UNLOCK_PIN = 18 # still need to solder and hook this up.
+MOTOR_SWITCH_PIN = 17 # Butterfly switch that signals when the lock is opened/closed.
 
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(MOTOR_LOCK_PIN, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(MOTOR_UNLOCK_PIN, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(MOTOR_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def cleanup():
+    GPIO.cleanup()
+
+def is_door_locked():
+    return GPIO.input(MOTOR_SWITCH_PIN)
+
+def lock_door(lock=True):
+    lock_pin = MOTOR_LOCK_PIN if lock else MOTOR_UNLOCK_PIN
+    end_time = time.time() + 4
+    # Run loop for 4 seconds or until door is at desired state, whichever comes first.
+    while lock != is_door_locked() and time.time() < end_time:
+        GPIO.output(4, GPIO.HIGH)
+    GPIO.output(4, GPIO.LOW)
+
+def __debug_is_door_locked():
+    current_state = is_door_locked()
+    try:
+        while True:
+            if current_state != is_door_locked():
+                current_state = is_door_locked()
+                if current_state:
+                    print('Door Locked')
+                else:
+                    print('Door unlocked')
+    except KeyboardInterrupt:
+        cleanup()
+
+if __name__ == '__main__':
+    setup()
+    __debug_is_door_locked()
