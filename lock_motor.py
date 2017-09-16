f1308c809b8391687bdbdad9bda8234699dfd4fd
gregpaton08 <gregpaton08@gmail.com>
2017-09-16 10:24:56 -0400

    Add script for lock motor logic

---
+++ lock_motor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+
+MOTOR_LOCK_PIN = 4
+MOTOR_UNLOCK_PIN = 18 # still need to solder and hook this up
+MOTOR_SWITCH_PIN = 17 # Butterfly switch that signals when the lock is opened/closed
+
