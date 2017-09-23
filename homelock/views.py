86a18deb96369f0e55a678ddba11e9727e0975a3
gregpaton08 <gregpaton08@gmail.com>
2017-09-23 17:37:07 -0400

    Add script and html

--- homelock/views.py
+++ homelock/views.py
@@ -1,6 +1,16 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 from homelock import app
+import lock_motor
 
 @app.route('/')
 def hello_world():
     return 'Hello, world!!!'
+
+@app.route('/lock_status')
+def lock_status():
+    return jsonify(locked=lock_motor.is_door_locked())
+
+@app.route('/lock')
+def lock():
+    print(request.args)
+    return 'hello_world'
