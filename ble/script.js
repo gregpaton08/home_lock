5cbfe3c4652cad7a102b3f31132c9cb6e97afd92
gregpaton08 <gregpaton08@gmail.com>
2017-10-03 14:23:38 -0400

    Add comments

--- ble/script.js
+++ ble/script.js
@@ -16,15 +16,20 @@ var characteristic = new Characteristic({
 
     onReadRequest: function(offset, callback) {
         console.log('Read request');
+
+        // TODO: replace with hardware check once connected to lock.
         var retVal = execSync('./lockStatus.sh');
-        console.log('RETVALE: ' + retVal);
+
         callback(Characteristic.RESULT_SUCCESS, Buffer(retVal == 1 ? 'FF' : '00', 'hex'));
     },
 
     onWriteRequest: function(newData, offset, withoutResponse, callback) {
         console.log('got newData: ' + newData.toString('utf8'));
+
+        // TODO: replace with direct call to hardware once connected to lock.
         exec('./toggleLock.sh', function(error, stdout, stderr) {
         });
+
         callback(bleno.Characteristic.RESULT_SUCCESS);
     }
 });
@@ -37,13 +42,13 @@ var primaryService = new PrimaryService({
 
 var services = [ primaryService ];
 bleno.on('advertisingStart', function(error) {
-    bleno.setServices( services );
+    bleno.setServices(services);
 });
 
 bleno.on('stateChange', function(state) {
     console.log('BLE stateChanged to: ' + state);
     if (state === 'poweredOn') {
-        bleno.startAdvertising('MyDevice',['fffffffffffffffffffffffffffffff0']);
+        bleno.startAdvertising('MyDevice', ['fffffffffffffffffffffffffffffff0']);
     } else {
         bleno.stopAdvertising();
     }
