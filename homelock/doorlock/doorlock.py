8dd29afca1318f477bc426d80dfe2b595ac0271e
gregpaton08 <gregpaton08@gmail.com>
2017-12-17 23:31:45 -0500

    Move test code into separate file

--- homelock/doorlock/doorlock.py
+++ homelock/doorlock/doorlock.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import RPi.GPIO as GPIO
 import time
 
@@ -40,43 +38,3 @@ class DoorLock:
 
     def get(self):
         return self.__lock_direction_clockwise != GPIO.input(self.__motor_switch_pin)
-
-
-def __debug_is_door_locked():
-    lock = DoorLock()
-    current_state = lock.get()
-    try:
-        while True:
-            if current_state != lock.get():
-                current_state = lock.get()
-                print('Door is {0}locked'.format('' if current_state else 'un'))
-            else:
-                time.sleep(0.1)
-    except KeyboardInterrupt:
-        pass
-
-def __debug_lock_door():
-    lock = DoorLock()
-    print('Debug lock_door()')
-    try:
-        while True:
-            input = raw_input('Enter \'L\' for lock or \'U\' for unlock: ')
-            status = None
-            if input is 'l':
-                status = True
-            elif input is 'u':
-                status = False
-            else:
-                print('Invalid input {0}'.format(input))
-                continue
-
-            if lock.set(status):
-                print('Door is {0}locked'.format('' if lock.get() else 'un'))
-            else:
-                print('ERROR: failed to {0}lock door'.format('' if status else 'un'))
-    except KeyboardInterrupt:
-        pass
-
-if __name__ == '__main__':
-    # __debug_is_door_locked()
-    __debug_lock_door()
