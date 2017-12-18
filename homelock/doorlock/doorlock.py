7eb693ce86d40ddf8b4176103a9609c09c492c22
gregpaton08 <gregpaton08@gmail.com>
2017-12-17 23:21:03 -0500

    Remove unused code

--- homelock/doorlock/doorlock.py
+++ homelock/doorlock/doorlock.py
@@ -8,36 +8,6 @@ MOTOR_UNLOCK_PIN = 18 # Orange wire out of lock. Pin to set high to unlock.
 MOTOR_SWITCH_PIN = 17 # Butterfly switch that signals when the lock is opened/closed.
 LOCK_DIRECTION_CLOCKWISE = True # Set depending on the lock direction: True if a clockwise spin locks or FALSE if a counter-clockwise spin locks.
 
-def setup():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(MOTOR_LOCK_PIN, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(MOTOR_UNLOCK_PIN, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(MOTOR_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
-def is_door_locked():
-    return LOCK_DIRECTION_CLOCKWISE != GPIO.input(MOTOR_SWITCH_PIN)
-
-def lock_door(lock=True):
-    if lock != is_door_locked():
-        lock_pin = MOTOR_LOCK_PIN if lock else MOTOR_UNLOCK_PIN
-        end_time = time.time() + 2 # seconds
-        # Run loop until the timeout is hit or until door is at desired state, whichever comes first.
-        GPIO.output(lock_pin, GPIO.HIGH)
-        time_out = False
-        while lock != is_door_locked() and not time_out:
-            time.sleep(0.1)
-            if time.time() > end_time:
-                time_out = True
-
-        # Sleep momentarily to allow the lock to fully turn.
-        if not time_out:
-            time.sleep(0.5)
-
-        GPIO.output(lock_pin, GPIO.LOW)
-
-    return lock == is_door_locked()
-
 class DoorLock:
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
@@ -73,21 +43,20 @@ class DoorLock:
 
 
 def __debug_is_door_locked():
-    current_state = is_door_locked()
+    lock = DoorLock()
+    current_state = lock.get()
     try:
         while True:
-            if current_state != is_door_locked():
-                current_state = is_door_locked()
-                if current_state:
-                    print('Door Locked')
-                else:
-                    print('Door unlocked')
+            if current_state != lock.get():
+                current_state = lock.get()
+                print('Door is {0}locked'.format('' if current_state else 'un'))
             else:
                 time.sleep(0.1)
     except KeyboardInterrupt:
-        pass#cleanup()
+        pass
 
 def __debug_lock_door():
+    lock = DoorLock()
     print('Debug lock_door()')
     try:
         while True:
@@ -101,12 +70,12 @@ def __debug_lock_door():
                 print('Invalid input {0}'.format(input))
                 continue
 
-            if lock_door(status):
-                print('Door is {0}locked'.format('' if is_door_locked() else 'un'))
+            if lock.set(status):
+                print('Door is {0}locked'.format('' if lock.get() else 'un'))
             else:
                 print('ERROR: failed to {0}lock door'.format('' if status else 'un'))
     except KeyboardInterrupt:
-        cleanup()
+        pass
 
 if __name__ == '__main__':
     setup()
