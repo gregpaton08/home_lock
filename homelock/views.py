4f4a3222f721cf505b92e3aa24091cb86af6e1c5
Gregory Paton <gregpaton08@gmail.com>
2019-05-19 08:00:21 -0400

    Adds files that were missing from previous two commits

--- homelock/views.py
+++ homelock/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from homelock import app
-from doorlock import DoorLock
+from doorlockcontroller import DoorLockController
 from flask_restful import Resource, Api
 import json
 
@@ -15,7 +15,7 @@ def hello_world():
 
 class LockAPI(Resource):
     def __init__(self):
-        self.lock = DoorLock()
+        self.lock = DoorLockController()
 
     def get(self):
         return { 'status' : self.lock.get() }
